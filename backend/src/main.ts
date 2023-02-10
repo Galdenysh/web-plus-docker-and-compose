4f970edd35d612407d9785a7decef596e11cc595
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-02-10 22:49:03 +0300

    feat: add setut docker in the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  const allowlist = [
+    process.env.CORS_URL_HTTP ?? 'http://localhost:3000',
+    process.env.CORS_URL_HTTPS ?? 'https://localhost:3000'
+  ];
+  const corsOptionsDelegate = (
+    req: any,
+    callback: (error: Error | null, corsOptions?: CorsOptions) => void,
+  ) => {
+    const corsOptions = { origin: false };
+    const header = req.header('Origin');
+
+    if (header !== undefined && allowlist.indexOf(header) !== -1) {
+      corsOptions.origin = true; // reflect (enable) the requested origin in the CORS response
+    }
+
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  };
+  app.enableCors(corsOptionsDelegate);
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
